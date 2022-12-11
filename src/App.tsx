1223b12d1a2fd842ee40688e561bd48b6fe7f407
Alexandre Carneiro <72991728+SDAlez@users.noreply.github.com>
2022-12-11 01:27:26 -0300

    Changed Design for Mobile

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,28 @@ import BarChart from './components/barChart';
 import { ThemeProvider } from '@emotion/react';
 import customTheme from "./components/customTheme"
 
+//height: 90%;
+//width: 36%
+
+const Chart = () => {
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    return <div className="ChartContainer" style={{"height": "89%", "width": "96%"}}>
+      <BarChart/>
+    </div>
+  } else {
+    return <div className="ChartContainer" style={{"height": "89%", "width": "36%"}}>
+      <BarChart/>
+    </div>
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={customTheme}>
         <AppNavBar />
       </ThemeProvider>
-      <div className="ChartContainer">
-        <BarChart />
-      </div>
+      <Chart/>
     </div>
   );
 }
